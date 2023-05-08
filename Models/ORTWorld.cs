2e1190f5ae12a32c2e46be9b44b2acc4edc5e323
AXEL NICOLÁS KLEIMAN <47434251@ort>
2023-05-08 09:06:06 -0300

    3

--- Models/ORTWorld.cs
+++ Models/ORTWorld.cs
@@ -1,9 +1,9 @@
 class ORTWorld
 {
-    public List<string> ListaDestinos{get;private set;}
-    public List<string> ListaHoteles{get;private set;}
-    public List<string> ListaAereo{get; private set;}
-    public List<string> ListaExcursiones{get; private set;}
+    public List<string> ListaDestinos{get;private set;} = new List<string>{"MIAMI", "TORONTO", "MISIONES", "DUBAI", "BEIJUNG", "TOKIO", "BRASILIA", "MEDELLIN", "CARACAS", "SUCRE"};
+    public List<string> ListaHoteles{get;private set;} = new List<string>{"BECHER", "REIFUT", "YUZE", "HUEVO", "IVO", "AXEL", "PARGA", "ZOBER", "MATA", "ZORRO"};
+    public List<string> ListaAereo{get; private set;} = new List<string>{"RIQUELME", "MARADONA", "PALERMO", "GUILLERMO", "BIANCHI", "TEVEZ", "BATTAGLIA", "IBARRA", "GATTI", "MOUZO"};
+    public List<string> ListaExcursiones{get; private set;} = new List<string>{"BOCA", "RIVER", "INDEPENDIENTE", "RACING", "SAN LORENZO", "ESTUDIANTES", "VELEZ", "NEWELLS", "ROSARIO", "ARGENTINOS"};
     public Dictionary<string, Paquete> Paquetes{get; private set;}
     public bool IngresarPaquete(string destinoSeleccionado, Paquete paquete)
     {
