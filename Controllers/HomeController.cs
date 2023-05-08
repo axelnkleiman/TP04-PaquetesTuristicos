8d29c77991db36a132cfe1a55e6a54aef2d40694
AXEL NICOLÁS KLEIMAN <47434251@ort>
2023-05-08 10:39:53 -0300

    3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,21 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
+        ViewBag.Paquetes = ORTWorld.Paquetes;
+        return View();
+    }
+    public IActionResult SelectPaquete()
+    {
+        ViewBag.ListaAereo = ORTWorld.ListaAereo;
+        ViewBag.ListaDestinos = ORTWorld.ListaDestinos;
+        ViewBag.ListaExcursiones = ORTWorld.ListaExcursiones;
+        ViewBag.ListaHoteles = ORTWorld.ListaHoteles;
+        return View();
+    }
+    public IActionResult GuardarPaquete(string Destino, string Hotel, string Aereo, string Excursion)
+    {
+        Paquete paquete = new Paquete(Hotel, Aereo, Excursion);
+        ORTWorld.IngresarPaquete(Destino, paquete);
         return View();
     }
 }
