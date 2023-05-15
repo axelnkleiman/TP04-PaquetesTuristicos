75d2ec2d6d9159af307bd7e5688820af626ad2db
AXEL NICOLÁS KLEIMAN <47434251@ort>
2023-05-15 09:03:13 -0300

    15/5/23

--- Models/ORTWorld.cs
+++ Models/ORTWorld.cs
@@ -1,10 +1,10 @@
 class ORTWorld
 {
-    public static List<string> ListaDestinos{get;private set;} = new List<string>{"MIAMI", "TORONTO", "MISIONES", "DUBAI", "BEIJUNG", "TOKIO", "BRASILIA", "MEDELLIN", "CARACAS", "SUCRE"};
+    public static List<string> ListaDestinos{get;private set;} = new List<string>{"MIAMI", "TORONTO", "MISIONES", "DUBAI", "BEIJING", "TOKIO", "BRASILIA", "MEDELLIN", "CARACAS", "SUCRE"};
     public static List<string> ListaHoteles{get;private set;} = new List<string>{"BECHER", "REIFUT", "YUZE", "HUEVO", "IVO", "AXEL", "PARGA", "ZOBER", "MATA", "ZORRO"};
-    public static List<string> ListaAereo{get; private set;} = new List<string>{"RIQUELME", "MARADONA", "PALERMO", "GUILLERMO", "BIANCHI", "TEVEZ", "BATTAGLIA", "IBARRA", "GATTI", "MOUZO"};
+    public static List<string> ListaAereo{get; private set;} = new List<string>{"Emirates", "CopaAir", "Latam", "Lufthansa", "AirChina", "Qatar Airways", "AeroMexico", "Aerolineas Argentinas", "British Airlines", "American Airlines"};
     public static List<string> ListaExcursiones{get; private set;} = new List<string>{"BOCA", "RIVER", "INDEPENDIENTE", "RACING", "SAN LORENZO", "ESTUDIANTES", "VELEZ", "NEWELLS", "ROSARIO", "ARGENTINOS"};
-    public static Dictionary<string, Paquete> Paquetes{get; private set;}
+    public static Dictionary<string, Paquete> Paquetes{get; private set;} = new Dictionary<string, Paquete>();
     public static bool IngresarPaquete(string destinoSeleccionado, Paquete paquete)
     {
         bool verificar;
